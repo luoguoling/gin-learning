3f0d459ba2e772681ea657829fce5065a4fa3e7c
luoguoling <992975991@qq.com>
2021-10-26 15:42:52 +0800

    Add files via upload

--- go.mod
+++ go.mod
@@ -1,9 +1,16 @@
-module newBubble
+module web_app
 
-go 1.16
+go 1.15
 
 require (
-	github.com/gin-gonic/gin v1.7.2
-	github.com/go-sql-driver/mysql v1.6.0
+	github.com/fsnotify/fsnotify v1.5.1
+	github.com/gin-gonic/gin v1.7.4
+	github.com/go-redis/redis/v8 v8.11.4
+	github.com/go-sql-driver/mysql v1.5.0
 	github.com/jinzhu/gorm v1.9.16
+	github.com/jmoiron/sqlx v1.3.4
+	github.com/natefinch/lumberjack v2.0.0+incompatible
+	github.com/spf13/viper v1.9.0
+	go.uber.org/zap v1.17.0
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,21 +1,104 @@
 package main
 
-//新版本
 import (
-	//"github.com/gin-gonic/gin"
-	"newBubble/dao"
-	"newBubble/models"
-	"newBubble/routers"
+	"context"
+	"flag"
+	"fmt"
+	//"github.com/spf13/viper"
+	"go.uber.org/zap"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+	"web_app/dao/mysql"
+	"web_app/dao/redis"
+	"web_app/logger"
+	"web_app/routes"
+	"web_app/settings"
 )
 
+//go web脚手架模板
 func main() {
-	err := dao.InitMySQL()
-	if err != nil {
-		panic(err)
+	//if len(os.Args) < 2 {
+	//	fmt.Println("请输入配置文件config")
+	//	return
+	//}
+	var filepath string
+	flag.StringVar(&filepath, "c", "filepath", "配置文件")
+	flag.Parse()
+	//1.加载配置文件
+	if err := settings.Init(); err != nil {
+		fmt.Printf("配置文件初始化失败,err:%v\n", err)
+		return
+	} else {
+		fmt.Println("配置文件加载成功!!!!")
 	}
-	defer dao.Close()
-	//模型绑定
-	dao.DB.AutoMigrate(&models.Todo{})
-	r := routers.SetupRouter()
-	r.Run(":9000")
+	//2.初始化配置
+	if err := logger.Init(settings.Conf.LogConfig); err != nil {
+		fmt.Printf("日志始化失败,err:%v\n", err)
+		return
+	}
+	zap.L().Sync() //缓存区日志追加到日志中
+	//3.初始化mysql连接
+	if err := mysql.Init(settings.Conf.MysqlConfig); err != nil {
+		fmt.Printf("mysql数据库始化失败,err:%v\n", err)
+		return
+	}
+	defer mysql.Close()
+
+	//3.1 初始化gorm连接
+	if err := mysql.InitGorm(settings.Conf.MysqlConfig); err != nil {
+		fmt.Printf("gorm数据库始化失败,err:%v\n", err)
+		return
+	}
+	defer mysql.GormClose()
+	mysql.Opertedb()
+	//4.初始化redis连接
+	if err := redis.Init(settings.Conf.RedisConfig); err != nil {
+		fmt.Printf("redis始化失败,err:%v\n", err)
+		return
+	} else {
+		fmt.Println("redis连接成功!!!")
+	}
+	defer redis.Close()
+	redis.Operatedb()
+	fmt.Println(settings.Conf.Name)
+	//5.注册路由
+	r := routes.Setup()
+	//6.启动服务(优雅关机)
+	srv := &http.Server{
+		//Addr:    fmt.Sprintf("%d", viper.GetInt("app.port")),
+		Addr:    fmt.Sprintf(":%s", settings.Conf.Port),
+		Handler: r,
+	}
+
+	go func() {
+		// 开启一个goroutine启动服务
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// 等待中断信号来优雅地关闭服务器，为关闭服务器操作设置一个5秒的超时
+	quit := make(chan os.Signal, 1) // 创建一个接收信号的通道
+	// kill 默认会发送 syscall.SIGTERM 信号
+	// kill -2 发送 syscall.SIGINT 信号，我们常用的Ctrl+C就是触发系统SIGINT信号
+	// kill -9 发送 syscall.SIGKILL 信号，但是不能被捕获，所以不需要添加它
+	// signal.Notify把收到的 syscall.SIGINT或syscall.SIGTERM 信号转发给quit
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM) // 此处不会阻塞
+	<-quit                                               // 阻塞在此，当接收到上述两种信号时才会往下执行
+	log.Println("Shutdown Server ...")
+	// 创建一个5秒超时的context
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	// 5秒内优雅关闭服务（将未处理完的请求处理完再关闭服务），超过5秒就超时退出
+	if err := srv.Shutdown(ctx); err != nil {
+		zap.L().Info("shutdown Server.....", zap.Error(err))
+		log.Fatal("Server Shutdown: ", err)
+	}
+
+	log.Println("Server exiting")
+	zap.L().Info("Server exiting")
 }
